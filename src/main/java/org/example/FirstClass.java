efa1e53ead6fc87b5892e19ea44932ccbeaa6a0b
chhagan lal <swamichhagan@gmail.com>
2025-06-19 13:13:50 -0700

    Initial commit

---
+++ src/main/java/org/example/FirstClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        String a="Chhagan";
+        System.out.println("Name is"+a);
+    }
+
+}
+// This class is part of the org.example package and contains a String variable 'a'.
+// The print statement is outside of any method, which will lead to a compilation error.
